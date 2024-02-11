2a9bca0ab4e9632252ada7d24bcac2302979e25a
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2024-02-11 12:29:12 +1100

    Transparent rectangles!

--- GameLib/ui/__init__.py
+++ GameLib/ui/__init__.py
@@ -1,13 +1,12 @@
 import pygame
 
 class Element():
-	def __init__(self, surface):
-		self.WINDOW = surface
+	def __init__(self, config):
+		self.WINDOW = config.get("surface", None)
 
 class Rectangle(Element):
-	
-	def __init__(self, surface, config):
-		Element.__init__(self, surface)
+	def __init__(self, config):
+		Element.__init__(self, config)
 
 		match config["coordSpace"]:
 			case "Center":
@@ -15,18 +14,38 @@ class Rectangle(Element):
 				config["posY"] -= config["sizeY"] / 2
 		# left, top, width, height
 
+		self.sizeX = config.get("sizeX", 50)
+		self.sizeY = config.get("sizeY", 20)
+
 		self.rect = pygame.Rect(config["posX"], config["posY"], config["sizeX"], config["sizeY"])
 		self.colour = config["Colour"]
 	
 	def update():
-		pygame.draw.rect(self.WINDOW, self.colour, self.rect, width=0)
+		#pygame.draw.rect(self.WINDOW, self.colour, self.rect, width=0)
+		draw_rect_alpha(self.WINDOW, self.colour, self.rect)
+		pass
+	
+	#def draw():
+	#	s = pygame.Surface((self.sizeX, self.sizeY), pygame.SRCALPHA, 32)
+	#	s.fill(self.colour)
+	#	return s
+
+	def draw_rect_alpha(surface, color, rect):
+		shape_surf = pygame.Surface(rect.size, pygame.SRCALPHA)
+		pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+		surface.blit(shape_surf, rect)
+
+	def rect(screen, x1, y1, x2, y2, alpha = 255):
+		#pygame.draw.rect(screen, (0,0,0), (x1,y1,x2,y2))
+		draw_rect_alpha(screen, (0, 0, 0, alpha), (x1, y1, x2, y2))
+
 
 
 
 class Button(Rectangle):
 
-	def __init__(self, surface, config):
-		Rectangle.__init__(self, surface, config)
+	def __init__(self, config):
+		Rectangle.__init__(self, config)
 		self.fontSize = config.get("fontSize", 12)
 		self.text = config.get("text", "")
 		self.style = config.get("style", "default")
@@ -47,12 +66,22 @@ class Button(Rectangle):
 			case "default":
 				pass
 		pass
+	
+	@staticmethod
+	def draw_button_alpha(surface, color, rect):
+		shape_surf = pygame.Surface(rect.size, pygame.SRCALPHA)
+		pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+		surface.blit(shape_surf, rect)
+
 
 	def update(self):
-		pygame.draw.rect(self.WINDOW, self.colour, self.rect, width=0)
+		
+		self.draw_button_alpha(self.WINDOW, self.colour, self.rect)
+		#pygame.draw.rect(self.WINDOW, self.colour, self.rect, width=0)
 		#if isMouseOver():
 			#highlight
 		#	if isPressed():
 				#fire event from Events package
 		#		pass
+
 
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import GameLib as gl
 
 
 # Colours
-BACKGROUND = (123, 123, 5)
+BACKGROUND = (0, 0, 0)
 
 # Game settings
 FPS = 60
@@ -15,23 +15,24 @@ WINDOW_HEIGHT = 450
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Moonlander 🚀')
 
-uiLayer = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+uiLayer = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA, 32)
+uiLayer = uiLayer.convert_alpha()
 
 
-config = {
+
+button1 = gl.ui.Button({
+    "surface": uiLayer,
     "coordSpace": "Center",
     "posX": 450,
     "posY": 250,
     "sizeX": 100,
     "sizeY": 50,
-    "Colour": pygame.Color("blue"),
+    "Colour": pygame.Color(0, 0, 255, 50),
     "fontSize": 5,
     "text": "hello",
     "style": "default",
     "font": "default",
-}
-
-button1 = gl.ui.Button(uiLayer, config)
+})
 
 def main():
     events()
@@ -41,7 +42,7 @@ def main():
 def draw():
     WINDOW.fill(BACKGROUND)
 
-    uiLayer.fill((255, 255, 255))  # Fill the UI layer with white
+    uiLayer.fill((255, 255, 255, 255))  # Fill the UI layer with white
     button1.update()
     WINDOW.blit(uiLayer, (0, 0))
 
